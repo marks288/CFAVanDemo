511c77ba7684aff261fa3535772b29294872b190
marks288 <167159399+marks288@users.noreply.github.com>
2024-06-11 15:26:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import streamlit as st
 
 st.title('DEMO')
 
-tickers = ['AAPL', 'DIS', 'NKE', 'NFLX']
+tickers = ['AAPL', 'DIS', 'NKE']
 pick = st.sidebar.selectbox("Pick ticker", tickers)
 st.write("You picked " + pick)
 
 startDate = st.sidebar.date_input("Pick a start date")
 endDate = st.sidebar.date_input("Pick an end date")
+
+df = pd.read_csv(+pick + ".csv")
+st.write(df)
