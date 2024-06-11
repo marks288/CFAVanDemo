b72bffeb5e274b127d5005f2a60f095ca31acfd1
marks288 <167159399+marks288@users.noreply.github.com>
2024-06-11 15:30:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
 st.title('DEMO')
 
@@ -10,5 +11,8 @@ st.write("You picked " + pick)
 startDate = st.sidebar.date_input("Pick a start date")
 endDate = st.sidebar.date_input("Pick an end date")
 
-df = pd.read_csv(pick + ".csv")
+df = pd.read_csv(pick + ".csv", parse_dates=['Date'])
 st.write(df)
+
+fig = px.line(df, x='Date', y='Close')
+st.plotly_chart(fig)
