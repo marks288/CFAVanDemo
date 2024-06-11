85a86941ba4825cf402f40826d9dfcd5601b5c77
marks288 <167159399+marks288@users.noreply.github.com>
2024-06-11 15:20:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 
 st.title('DEMO')
 
-tickers = ['AAPL', 'NFLX']
-pick = st.selectbox("Pick ticker", tickers)
+tickers = ['AAPL', 'DIS', 'NKE', 'NFLX']
+pick = st.sidebar.selectbox("Pick ticker", tickers)
 st.write("You picked " + pick)
+
+startDate = st.date_input("Pick a start date")
