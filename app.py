3eb850af6159ce97c27e131d81a2b8f781ef32c7
marks288 <167159399+marks288@users.noreply.github.com>
2024-06-11 15:22:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ tickers = ['AAPL', 'DIS', 'NKE', 'NFLX']
 pick = st.sidebar.selectbox("Pick ticker", tickers)
 st.write("You picked " + pick)
 
-startDate = st.date_input("Pick a start date")
+startDate = st.sidebar.date_input("Pick a start date")
+endDate = st.sidebar.date_input("Pick an end date")
